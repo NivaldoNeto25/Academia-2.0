119333c0107b76289f486aa7f4396e9374419a0e
Maria Milena <necimalaquias20@gmail.com>
2025-10-03 09:22:12 -0300

    #63

--- academia2.0/src/main/java/br/upe/academia2/data/repository/interfaces/IIndBioRepository.java
+++ academia2.0/src/main/java/br/upe/academia2/data/repository/interfaces/IIndBioRepository.java
@@ -2,7 +2,6 @@ package br.upe.academia2.data.repository.interfaces;
 
 import br.upe.academia2.data.beans.IndicadorBiomedico;
 import java.util.List;
-import java.util.Optional;
 
 public interface IIndBioRepository {
     boolean save(IndicadorBiomedico indicadorBiomedico);
